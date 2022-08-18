db6b690d7021d7ef4ab9d043394a8dae70747375
José Pach <Paching12@icloud.com>
2022-08-18 01:10:01 -0500

    Progress: unit of measure

--- 2-30.cpp
+++ 2-30.cpp
@@ -13,25 +13,32 @@
 using namespace std;
 
 // Prototipos
-void printMenu(void);
+int getUnitMeasure(void);
 double getWeight(void);
 double getHeight(void);
-double getBMI(double, double);
+double getBMI(double, double, int);
+void printMenu(void);
 
 // Constants
 const int POUNDS = 703;
 
 int main() {
+
+    int unitMeasure = 0;
     double weight = 0.0;
     double height = 0.0;
 
+    // Print info
     printMenu();
+
+    // Get type of measure
+    unitMeasure = getUnitMeasure();
 
     // Get weight and height
     weight = getWeight();
     height = getHeight();
 
-    cout << "BMI calculated = " << getBMI(weight, height) << endl;
+    cout << "BMI calculated = " << getBMI(weight, height, unitMeasure) << endl;
 
     return 0;
 }
@@ -65,6 +72,15 @@ double getHeight() {
 }
 
 // This function gets BMI with weight and hight
-double getBMI(double weight, double height) {
-    return (weight * (double) POUNDS) /  (height * height);
+double getBMI(double weight, double height, int unitMeasure) {
+    return (weight * (unitMeasure ? (double) POUNDS * 1 : 1)) /  (height * height);
+}
+
+int getUnitMeasure(void) {
+    int unitMeasure = 0; 
+    cout << "Type 0 for meters/kilograms" << endl; 
+    cout << "Type 1 for pounds/inches" << endl; 
+    cout << "Please select your unit of measurement (0 = mt/kg, 1 = lb/in)" << endl; 
+    cin >> unitMeasure;
+    return unitMeasure;
 }
